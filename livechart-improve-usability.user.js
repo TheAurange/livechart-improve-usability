7ade9e930fe598e31ea972ee459177dab2ef2bf2
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-01-01 03:03:50 -0600

    Update livechart-improve-usability.user.js

--- livechart-improve-usability.user.js
+++ livechart-improve-usability.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name    [LiveChart] Improve Usability
 // @author  Aurange
-// @version 1.5
+// @version 1.6
 // @match   https://www.livechart.me/*-*/*
 // @match   https://www.livechart.me/tba/*
 // ==/UserScript==
@@ -28,36 +28,22 @@ new MutationObserver(function(mutationList, observer){
     observer.disconnect();
 
     document.querySelectorAll("div.anime-episodes").forEach(e => {
-      if(e.parentElement.parentElement.parentElement.parentElement.style.display !== "none" && e.parentElement.parentElement.parentElement.children[2].children[0].innerText.indexOf("EP") !== -1 && e.innerText.split(" eps")[0].indexOf("?") === -1 && e.innerText.split(" eps").length > 1) {
-        let fixer = e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split(": ")[1].split(" "),
-            eC = e.innerText.split(" eps")[0] - e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split("EP")[1].split(":")[0] + 1,
-            pluralizer = (eC > 1) ? "s" : "",
+      if(e.parentElement.parentElement.parentElement.parentElement.style.display !== "none" && e.parentElement.parentElement.parentElement.children[2].children[0].innerText.indexOf("EP") !== -1 && e.innerText.split(" eps")[0].indexOf("?") === -1 && e.innerText.split(" eps").length > 1){
+        let count = e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split(": ")[1].split(" "),
+            end,
+            eL = e.innerText.split(" eps")[0] - e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split("EP")[1].split(":")[0] + 1,
+            pluralizer = (eL > 1) ? "s" : "",
             info = document.createElement("p");
 
-        if(e.parentElement.parentElement.children[1].innerText.indexOf("Began") === -1){
-          let end = new Date(e.parentElement.parentElement.children[1].innerText.replace(" at", ",").replace(/(am|pm)/, " $&"));
+        count = (count[0].slice(0, -1) * 86400000) + (count[1].slice(0, -1) * 3600000) + (count[2].slice(0, -1) * 60000) + (count[3].slice(0, -1) * 1000);
 
-          fixer = Math.ceil(((fixer[0].slice(0, -1) * 86400000) + (fixer[1].slice(0, -1) * 3600000) + (fixer[2].slice(0, -1) * 60000) + (fixer[3].slice(0, -1) * 1000)) / 604800000);
+        end = new Date(Date.now() + count);
 
-          end.setDate(end.getDate() + ((e.innerText.split(" eps")[0] - 1) * 7));
-          end.setDate(end.getDate() + ((eC - Math.ceil((end - new Date()) / 604800000)) * 7));
+        end.setDate(end.getDate() + ((eL - 1) * 7));
 
-          if(fixer > 1) end.setDate(end.getDate() + ((fixer - 1) * 7));
+        info.append("This show ends on " + end.toLocaleDateString() + ". (" + eL + " Episode" + pluralizer + " Left)");
 
-          info.append("This show ends on " + end.toLocaleDateString() + ". (" + eC + " Episode" + pluralizer + " Left)");
-          e.parentElement.parentElement.children[3].prepend(info);
-        }
-        else{
-          let current = new Date();
-
-          current.setDate(current.getDate() - (e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split("EP")[1].split(":")[0] * 7));
-          current.setDate(current.getDate() + ((e.innerText.split(" eps")[0] - 1) * 7));
-          current.setDate(current.getDate() + ((eC - Math.ceil((current - new Date()) / 604800000)) * 7));
-          current.setTime(current.getTime() - (604800000 - ((fixer[0].slice(0, -1) * 86400000) + (fixer[1].slice(0, -1) * 3600000) + (fixer[2].slice(0, -1) * 60000) + (fixer[3].slice(0, -1) * 1000))));
-
-          info.append("This show ends on " + current.toLocaleDateString() + ". (" + eC + " Episode" + pluralizer + " Left)");
-          e.parentElement.parentElement.children[3].prepend(info);
-        }
+        e.parentElement.parentElement.children[3].prepend(info);
       }
     });
   }
