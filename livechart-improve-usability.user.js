fdf8bd8419f612e897fb20454fc169aaa26bafe5
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-08-26 15:57:12 -0500

    Update livechart-improve-usability.user.js

--- livechart-improve-usability.user.js
+++ livechart-improve-usability.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name    [LiveChart] Improve Usability
 // @author  Aurange
-// @version 1.10
+// @version 1.11
 // @match   https://www.livechart.me/*-*/*
 // @match   https://www.livechart.me/tba/*
 // ==/UserScript==
@@ -23,26 +23,14 @@ new MutationObserver(function(mutationList, observer){
   if(document.querySelector("div.poster-container")){
     observer.disconnect();
 
-    //Begin hide movies section.
-    document.querySelectorAll("p.editor-note").forEach(e => {
-      if(e.innerText === "※ NOTE: Theatrical Premiere") e.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+    //Begin hide theatrical releases section.
+    document.querySelectorAll("div.anime-card").forEach(e => {
+      if(e.querySelector("div.anime-episodes").innerText.indexOf("×") === -1 && e.querySelector("div.anime-synopsis").innerText.indexOf("※ NOTE: BD & DVD Release") === -1) e.parentElement.style.display = "none";
     });
-
-    document.querySelectorAll("time[data-label='THTR']").forEach(e => {
-      e.parentElement.parentElement.parentElement.style.display = "none";
-    });
+    //End hide theatrical releases section.
 
     document.querySelectorAll("div.anime-episodes").forEach(e => {
-      if(e.innerText.indexOf("×") === -1 && e.parentElement.parentElement.children[3].children[0].innerText !== "※ NOTE: BD/DVD Release") e.parentElement.parentElement.parentElement.parentElement.style.display = "none";
-    });
-
-    document.querySelectorAll("a[data-anime-card-target='mainTitle']").forEach(e => {
-      if(e.innerText.indexOf("Film") !== -1) e.parentElement.parentElement.parentElement.style.display = "none";
-    });
-    //End hide movies section.
-
-    document.querySelectorAll("div.anime-episodes").forEach(e => {
-      if(e.parentElement.parentElement.parentElement.parentElement.style.display !== "none" && e.parentElement.parentElement.parentElement.children[2].children[0].innerText.indexOf("EP") !== -1 && e.innerText.split(" eps")[0].indexOf("?") === -1 && e.innerText.split(" eps").length > 1){
+      if(e.parentElement.parentElement.parentElement.parentElement.style.display !== "none" && e.parentElement.parentElement.parentElement.children[2].children[0].innerText.indexOf("EP") !== -1 && e.innerText.split(" eps")[0].indexOf("?") === -1){
         let count = e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split(": ")[1].split(" "),
             end,
             eL = e.innerText.split(" eps")[0] - e.parentElement.parentElement.parentElement.children[2].children[0].innerText.split("EP")[1].split(":")[0] + 1,
@@ -53,7 +41,7 @@ new MutationObserver(function(mutationList, observer){
 
         end.setDate(end.getDate() + ((eL - 1) * 7));
 
-        info.append("This show ends on " + end.toLocaleDateString() + ". (" + eL + " Episode" + pluralizer + " Left)");
+        info.innerText = `This show ends on ${end.toLocaleDateString()}. (${eL} Episode${pluralizer} Left)`;
 
         e.parentElement.parentElement.children[3].prepend(info);
       }
